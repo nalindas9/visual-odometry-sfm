b3cf276ca0c4d0b6bfe484c91d36a6ea715d3ebb
akhopkar01 <aadi0110@gmail.com>
2020-04-29 00:20:21 -0400

    Preparing pipeline

--- Code/DataPreprocess.py
+++ Code/DataPreprocess.py
@@ -3,19 +3,26 @@ import cv2
 import glob
 import ReadCameraModel as r
 import UndistortImage as udi
+import os
 
 file_path = "/home/aditya/Oxford_dataset/stereo/centre/*"
 camera_path = "/home/aditya/Oxford_dataset/model"
+write_path = "/home/aditya/PycharmProjects/VisualOdometry/Undistorted"
 
 undistorted = []
 fx, fy, cx, cy, G_camera_img, LUT = r.ReadCameraModel(camera_path)
-
+Frame = 0
+print("Images are loading.. ")
 for fname in sorted(glob.glob(file_path)):
-    print(fname)
+    img_name = fname.split("/")[-1]
+    print("Loading:", img_name)
     raw = cv2.imread(fname, 0)
     color_img = cv2.cvtColor(raw, cv2.COLOR_BayerGR2BGR)
     undistort_img = udi.UndistortImage(color_img, LUT)
+    write_name = "undistorted_"+img_name
     img = cv2.resize(undistort_img, (0,0), fx=0.5, fy=0.5)
+    cv2.imwrite(os.path.join(write_path, write_name), img)
+    Frame += 1
     undistorted.append(img)
-
+print("Images loaded !")
 CameraModel = [fx, fy, cx, cy, G_camera_img, LUT]
--- Code/main.py
+++ Code/main.py
@@ -1,55 +1,191 @@
-"""
-Visual Odomotry SFM
-
-Authors:
-Nalin Das (nalindas9@gmail.com)
-Graduate Student pursuing Masters in Robotics,
-University of Maryland, College Park
-"""
-import glob
+# import DataPreprocess as pp
 import cv2
-import ReadCameraModel
-import UndistortImage
-from matplotlib import pyplot as plt 
+import glob
+import numpy as np
+from cv2 import xfeatures2d
+import random
+random.seed(1)
+# processed = pp.undistorted
+# cam_model = pp.CameraModel
 
-# Specify the path for all the video frames here
-IMAGES_PATH = "/home/nalindas9/Documents/Courses/Spring_2020_Semester_2/ENPM673_Perception_for_Autonomous_Robots/Github/enpm673/Oxford_dataset/stereo/centre"
-# Specify the path for the camera parameters
-MODELS_PATH = "/home/nalindas9/Documents/Courses/Spring_2020_Semester_2/ENPM673_Perception_for_Autonomous_Robots/Github/enpm673/Oxford_dataset/model"
+path = "/home/aditya/PycharmProjects/VisualOdometry/Undistorted/*"
 
-def featurematch(frame1, frame2):
-  # Using SIFT to find keypoints and descriptors
-  orb = cv2.ORB_create()
-  kp1, des1 = orb.detectAndCompute(frame1,None)
-  kp2, des2 = orb.detectAndCompute(frame2,None)
-  bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-  matches = bf.match(des1,des2)
-  matches = sorted(matches, key = lambda x:x.distance)
-  img3 = cv2.drawMatches(frame1,kp1,frame2,kp2,matches[:20], None, flags=2)
-  plt.imshow(img3),plt.show()
-  
-  
-# Main Function
-def main():
-  # Extract the camera params
-  fx, fy, cx, cy, G_camera_image, LUT = ReadCameraModel.ReadCameraModel(MODELS_PATH)
-  print('The extracted camera parameters are fx = {:}, fy = {:}, cx = {:}, cy = {:}, G_camera_image = {:}, LUT = {:}:'.format(fx, fy, cx, cy, G_camera_image, LUT))
-  successive_frames = []
-  itr = 0
-  # Iterating through all frames in the video and doing some preprocessing
-  for frame in sorted(glob.glob(IMAGES_PATH + "/*")):
-    print('Image:', frame.split("centre/", 1)[1])
-    img = cv2.imread(frame,0)
-    img = cv2.cvtColor(img, cv2.COLOR_BayerGR2BGR)
-    img = UndistortImage.UndistortImage(img,LUT)
-    img = cv2.resize(img, (0,0),fx=0.5,fy=0.5)
-    successive_frames.append(img)
-    if itr != 0 and itr%2 == 0:
-      featurematch(successive_frames[0], successive_frames[1])
-      successive_frames = []        
-    cv2.imshow('Color image', img)
-    cv2.waitKey(0)
-    itr = itr + 1
-    
-if __name__ == '__main__':
-  main()
+
+# processed = []
+# for fname in sorted(glob.glob(path)):
+#     print(fname.split("/")[-1])
+#     img = cv2.imread(fname)
+#     processed.append(img)
+
+
+def getFeatureMatches(img1, img2):
+    """
+    Uses keypoint algorithm to extract feature points from the image and get point correspondences
+    :param: img1 (Left image), img2 (Right image)
+    :return: ndarray of (x,y)L and (x,y)R feature points
+    """
+    orb = xfeatures2d.SIFT_create()
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    index_params = dict(algorithm=1, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, 2)
+    Left_Pts = list()
+    Right_Pts = list()
+
+    # Ratio criteria according to Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.75 * n.distance:
+            Left_Pts.append(kp2[m.trainIdx].pt)
+            Right_Pts.append(kp1[m.queryIdx].pt)
+
+    left = np.int32(Left_Pts)
+    right = np.int32(Right_Pts)
+    features = (left, right)
+    return features
+
+
+def computeFundamentalMatrix(pts1, pts2):
+    """
+    This function computes the fundamental matrix by computing the SVD of Ax = 0
+    :param pts1: ndarray left feature points
+    :param pts2: ndarray right feature points
+    :return: F(3x3) matrix of rank 2
+    """
+    x1 = pts1[:, 0]
+    y1 = pts1[:, 1]
+    x2 = pts2[:, 0]
+    y2 = pts2[:, 1]
+    n = x1.shape[0]
+    A = np.zeros((n, 9))
+    for i in range(0, n):
+        A[i] = [x1[i] * x2[i], x1[i] * y2[i], x1[i],
+                y1[i] * x2[i], y1[i] * y2[i], y1[i],
+                x2[i], y2[i], 1]
+    # Compute F matrix by evaluating SVD
+    U, S, V = np.linalg.svd(A)
+    F = V[-1].reshape(3, 3)
+
+    # Constrain the F matrix to rank 2
+    U, S, V = np.linalg.svd(F)
+    S[2] = 0
+    F = np.dot(U, np.dot(np.diag(S), V))
+    return F / F[2, 2]
+
+
+def getBestfitFundamentalMatrix(pts1, pts2):
+    """
+    Leverages the 8-point algorithm and implement RANSAC algorithm to find the best F matrix
+    :param pts1: ndarray of left features
+    :param pts2: ndarray of right features
+    :return: F(3x3) matrix
+    """
+    # global finalFundamentalMatrix
+    n = 0
+    iterations = 500
+    threshold = 0.05
+    max_count = 0
+    for i in range(0, iterations):
+        count = 0
+        idx = random.sample(range(8), 8)
+        left_pts = pts1[idx]
+        right_pts = pts2[idx]
+        F = computeFundamentalMatrix(left_pts, right_pts)
+        left_feature_inlier = []
+        right_feature_inlier = []
+        for j in range(0, len(idx)):
+            homogeneous_right = np.array([right_pts[j, 0], right_pts[j, 1], 1])
+            homogeneous_left = np.array([left_pts[j, 0], left_pts[j, 1], 1])
+            fit = np.dot(homogeneous_right.T, np.dot(F, homogeneous_left))
+            if fit < threshold:
+                left_feature_inlier.append(left_pts)
+                right_feature_inlier.append(right_pts)
+                count += 1
+        print('Inlier count', count)
+        if count > max_count:
+            max_count = count
+            finalFundamentalMatrix = F
+            inlier_Left = left_feature_inlier
+            inlier_Right = right_feature_inlier
+    return finalFundamentalMatrix
+
+
+def getEssentialMatrix(K, F):
+    """
+    This function computes the essential matrix from the fundamental matrix. The E matrix is defined
+    in normalized image coordinates
+    :param K: camera calibration matrix
+    :param F: best fitted Fundamental matrix
+    :return: Essential Matrix
+    """
+    E = np.dot(K.T, np.dot(F, K))
+    u, s, v = np.linalg.svd(E)
+
+    # We correct the singular values of the E matrix
+    s_new = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]]).reshape(3,3)
+    final_E = np.dot(u, np.dot(s_new, v.T))
+    return final_E
+
+
+def ExtractCameraPose(E):
+    """
+    Given the essential matrix, we derive the camera position and orientation
+    :param E: Essential Matrix (3x3)
+    :return: list(rotations), list(position)
+    """
+    u, s, v = np.linalg.svd(E)
+    w = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]).reshape(3,3)
+    c1 = u[:, 2]
+    r1 = np.dot(u, np.dot(w, v.T))
+    c2 = u[:, 2]
+    r2 = np.dot(u, np.dot(w, v.T))
+    c3 = u[:, 2]
+    r3 = np.dot(u, np.dot(w.T, v.T))
+    c4 = -u[:, 2]
+    r4 = np.dot(u, np.dot(w.T, v.T))
+    if np.linalg.det(r1) < 0:
+        c1 = -c1
+        r1 = -r1
+    if np.linalg.det(r2) < 0:
+        c2 = -c2
+        r2 = -r2
+    if np.linalg.det(r3) < 0:
+        c3 = -c3
+        r3 = -r3
+    if np.linalg.det(r4) < 0:
+        c4 = -c4
+        r4 = -r4
+    cam_pose = ([c1, c2, c3, c4], [r1, r2, r3, r4])
+    return cam_pose
+
+
+
+img1 = cv2.imread("/home/aditya/PycharmProjects/VisualOdometry/Undistorted/undistorted_1399381446204705.png")
+img2 = cv2.imread("/home/aditya/PycharmProjects/VisualOdometry/Undistorted/undistorted_1399381446704623.png")
+features2d = getFeatureMatches(img1, img2)
+left_features = features2d[0]
+right_features = features2d[1]
+print('Match points:', left_features[:8])
+F = computeFundamentalMatrix(left_features[:8], right_features[:8])
+print(np.linalg.matrix_rank(F))
+idx = random.sample(range(8), 8)
+print(idx)
+print(left_features[idx], right_features[idx])
+F2 = computeFundamentalMatrix(left_features[idx], right_features[idx])
+print(F2)
+left = left_features[idx]
+right = right_features[idx]
+nleft = np.array([left[0,0], left[0,1], 1])
+nright = np.array([right[0,0], right[0,1], 1])
+print(nleft, nright)
+fit = np.dot(nright.T, np.dot(F, nleft))
+print(fit)
+Ff = getBestfitFundamentalMatrix(left_features, right_features)
+print(Ff)
+# img1 = cv2.drawKeypoints(img1, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# print(len(kp))
+# cv2.imshow("First Ref", img1)
+# cv2.imshow("Second ref", img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
