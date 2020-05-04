f6b105b676f868527662a7b73d8d6cbea9f22c83
akhopkar01 <aadi0110@gmail.com>
2020-05-04 04:32:53 -0400

    Final changes to the pipeline methods

--- Code/MyUtils.py
+++ Code/MyUtils.py
@@ -3,72 +3,75 @@ from cv2 import xfeatures2d
 import random
 import cv2
 
-#### THIS CODE FILE CONTAINS THE UTILITIES TO IMPLEMENT VISUAL ODOMETRY ####
+random.seed(1)
+### THIS CODE FILE CONTAINS THE UTILITIES TO IMPLEMENT VISUAL ODOMETRY ###
 
 
 def getFeatureMatches(img1, img2):
     """
-    Uses keypoint algorithm to extract feature points from the image and get point correspondences
+    Uses keypoint algorithm SIFT to extract feature points from the image and get point correspondences
     :param: img1 (Left image), img2 (Right image)
-    :return: ndarray of (x,y)L and (x,y)R feature points
+    :return: tuple of ndarray of (x,y)L and (x,y)R feature points
     """
-    orb = xfeatures2d.SIFT_create()
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, None)
-    index_params = dict(algorithm=1, trees=5)
+    sift = xfeatures2d.SIFT_create()
+
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)
+
     flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1, des2, 2)
+    matches = flann.knnMatch(des1, des2, k=2)
     Left_Pts = list()
     Right_Pts = list()
 
     # Ratio criteria according to Lowe's paper
     for i, (m, n) in enumerate(matches):
         if m.distance < 0.5 * n.distance:
-            Left_Pts.append(kp2[m.trainIdx].pt)
-            Right_Pts.append(kp1[m.queryIdx].pt)
+            Left_Pts.append(kp1[m.queryIdx].pt)
+            Right_Pts.append(kp2[m.trainIdx].pt)
 
-    left = np.int32(Left_Pts)
-    right = np.int32(Right_Pts)
+    left = np.array(Left_Pts)
+    right = np.array(Right_Pts)
     features = (left, right)
     return features
 
 
 def computeFundamentalMatrix(pts1, pts2):
     """
-    This function computes the fundamental matrix by computing the SVD of Ax = 0
+    This function computes the fundamental matrix by computing the SVD of Ax = 0 ; 8-point algorithm
     :param pts1: ndarray left feature points
     :param pts2: ndarray right feature points
     :return: F(3x3) matrix of rank 2
     """
-    x1 = pts1[:, 0]
-    y1 = pts1[:, 1]
-    x2 = pts2[:, 0]
-    y2 = pts2[:, 1]
-    n = x1.shape[0]
-    A = np.zeros((n, 9))
-    for i in range(0, n):
-        A[i] = [x1[i] * x2[i], x1[i] * y2[i], x1[i],
-                y1[i] * x2[i], y1[i] * y2[i], y1[i],
-                x2[i], y2[i], 1]
+    A = np.empty((8, 9))
+    for i in range(len(pts1)-1):
+        x1 = pts1[i][0]
+        x2 = pts2[i][0]
+        y1 = pts1[i][1]
+        y2 = pts2[i][1]
+        A[i] = np.array([x1 * x2, x2 * y1, x2,
+                         y2 * x1, y2 * y1, y2,
+                         x1, y1, 1])
     # Compute F matrix by evaluating SVD
     U, S, V = np.linalg.svd(A)
     F = V[-1].reshape(3, 3)
 
     # Constrain the F matrix to rank 2
-    U, S, V = np.linalg.svd(F)
+    U1, S1, V1 = np.linalg.svd(F)
     # print('Old S', S)
     # S[2] = 0
-    S2 = np.array([[S[0], 0, 0], [0, S[1], 0], [0, 0, 0]])
+    S2 = np.array([[S1[0], 0, 0], [0, S1[1], 0], [0, 0, 0]])
     # print('New S', S)
-    F = np.dot(np.dot(U, S2), V)
+    F = np.dot(np.dot(U1, S2), V1)
 
     return F
 
-
 def getInlierRANSAC(pts1, pts2):
     """
-    Leverages the 8-point algorithm and implement RANSAC algorithm to find the inliers
+    Leverages the 8-point algorithm and implement RANSAC algorithm to find the inliers and the best fundamental matrix
     :param pts1: ndarray of left features
     :param pts2: ndarray of right features
     :return: left inliers and Right inliers
@@ -78,14 +81,18 @@ def getInlierRANSAC(pts1, pts2):
     threshold = 0.01
     max_count = 0
     n = len(pts1)
-    for i in range(0, iterations):
+    finalFundamentalMatrix = np.zeros((3, 3))
+
+    for i in range(iterations):
         count = 0
-        idx = random.sample(range(n), 8)
+        idx = random.sample(range(n - 1), 8)
         left_pts = pts1[idx]
         right_pts = pts2[idx]
+
         F = computeFundamentalMatrix(left_pts, right_pts)
         left_feature_inlier = []
         right_feature_inlier = []
+
         # print("Sample index: ", len(idx))
         for j in range(0, n):
             homogeneous_right = np.array([pts2[j, 0], pts2[j, 1], 1])
@@ -95,17 +102,17 @@ def getInlierRANSAC(pts1, pts2):
             if np.abs(fit) < threshold:
                 left_feature_inlier.append(pts1[j])
                 right_feature_inlier.append(pts2[j])
-                count += 1
+                count = count + 1
         # print('Inlier count', count)
         inlier_Left = np.array(left_feature_inlier)
         inlier_Right = np.array(right_feature_inlier)
-        # betterF = computeFundamentalMatrix(inlier_Left, inlier_Right)
+
         if count > max_count:
             max_count = count
             finalFundamentalMatrix = F
             final_inlier_Left = inlier_Left
             final_inlier_Right = inlier_Right
-    return final_inlier_Left, final_inlier_Right, finalFundamentalMatrix
+    return finalFundamentalMatrix, final_inlier_Left, final_inlier_Right
 
 
 def getEssentialMatrix(K, F):
@@ -120,7 +127,7 @@ def getEssentialMatrix(K, F):
     u, s, v = np.linalg.svd(E)
 
     # We correct the singular values of the E matrix
-    s_new = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]]).reshape(3,3)
+    s_new = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]]).reshape(3, 3)
     final_E = np.dot(u, np.dot(s_new, v))
     return final_E
 
@@ -132,7 +139,9 @@ def ExtractCameraPose(E):
     :return: list(rotations), list(position)
     """
     u, s, v = np.linalg.svd(E, full_matrices=True)
-    w = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]).reshape(3, 3)
+    w = np.array([[0, -1, 0],
+                  [1, 0, 0],
+                  [0, 0, 1]]).reshape(3, 3)
     c1 = u[:, 2].reshape(3, 1)
     r1 = np.dot(np.dot(u, w), v).reshape(3, 3)
     c2 = -u[:, 2].reshape(3, 1)
@@ -168,7 +177,7 @@ def getTriangulationPoint(K, M, left_point, right_point):
     :return: P (point in 3D plane)
     """
     R_origin = np.eye(3)
-    C_origin = np.array([[0],[0],[0]])
+    C_origin = np.array([[0], [0], [0]])
 
     Cam_Origin = K @ np.hstack((R_origin, -R_origin @ C_origin))
 
@@ -178,21 +187,22 @@ def getTriangulationPoint(K, M, left_point, right_point):
     m3_dash = M[2, :]
     m1_dash = M[0, :]
     m2_dash = M[1, :]
+
     x, y = left_point[0], left_point[1]
     x_dash, y_dash = right_point[0], right_point[1]
-    a1 = x*m3 - m1
-    a2 = y*m3 - m2
-    a3 = x_dash*m3_dash - m1_dash
-    a4 = y_dash*m3_dash - m2_dash
+    a1 = x * m3 - m1
+    a2 = y * m3 - m2
+    a3 = x_dash * m3_dash - m1_dash
+    a4 = y_dash * m3_dash - m2_dash
     A = np.vstack((a1, a2, a3, a4))
     # print(A.shape)
     u, s, v = np.linalg.svd(A)
 
-    X = v[3]
-    X = X/X[-1]
+    Xn = v[3]
+    Xn = Xn / Xn[-1]
 
     # print(X)
-    return X.reshape((4, 1))
+    return Xn.reshape((4, 1))
 
 
 def getExtrinsicParameter(K, R, C):
@@ -211,7 +221,8 @@ def getExtrinsicParameter(K, R, C):
 
 def getDisambiguousPose(K, C, R, left_features, right_features):
     """
-    Gets the translation vector and rotation matrix of the camera
+    Gets the translation vector and rotation matrix of the camera w.r.t the world frame
+    and removes camera frame ambiguity
     :param K: Camera intrinsic matrix
     :param C: Camera center ndarray
     :param R: Camera Rotation Matrix ndarray
@@ -222,13 +233,12 @@ def getDisambiguousPose(K, C, R, left_features, right_features):
     check = 0
     for i in range(0, len(R)):
         count = 0
-        # extrinsic_params = np.vstack((np.hstack((R[i].reshape(3, 3), C[i])), np.array([0, 0, 0, 1])))
         extrinsic_params = getExtrinsicParameter(K, R[i], C[i])
         for j in range(0, len(left_features)):
             X = getTriangulationPoint(K, extrinsic_params, left_features[j], right_features[j])
             r3 = R[i][2, :].reshape((1, 3))
             cheiralityCondition = np.dot(r3, X[:3] - C[i])
-            if cheiralityCondition > 0 and X[2]>0:
+            if cheiralityCondition > 0 and X[2] >= 0:
                 count += 1
         if count > check:
             check = count
